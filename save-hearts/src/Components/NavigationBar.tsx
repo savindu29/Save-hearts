676f4a19e1842bc6c02e47da3706fadae5ab2b21
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-02-16 00:19:26 +0530

    Create navigation bar component

--- save-hearts/src/Components/NavigationBar.tsx
+++ save-hearts/src/Components/NavigationBar.tsx
@@ -1,19 +1,23 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import "./NavigationBar.scss"
 
 export function NavigationBar() {
 
     return (
         <div className="navigation-bar">
-            <Navbar bg="light" expand="lg">
+            <Navbar expand="lg">
                 <Container>
                     <Navbar.Brand href="#home"><img src="/assets/images/logo/save-hearts-logo.png"
                                                     alt="save-hearts-logo" width={175}/></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
+                        <Nav className="ms-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="#link" className={"mx-2"}>Statistics</Nav.Link>
+                            <Nav.Link href="#link" className={"mx-2"}>Organize a Camp</Nav.Link>
+                            <Nav.Link href="#link" className={"mx-2"}>Reserve a Time</Nav.Link>
+                            <Nav.Link href="#link" className={"mx-2"}>Contact Us</Nav.Link>
+                            <Nav.Link href="#link"><Button className={"px-4"}>Sign in</Button></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
